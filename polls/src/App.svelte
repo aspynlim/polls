cceaa49adeb058b5390ff457e70d51e4e3ab6bf0
JenLim <cmdlhz@gmail.com>
2021-07-24 15:42:31 +0900

    User Input & Data Binding

--- polls/src/App.svelte
+++ polls/src/App.svelte
@@ -1,14 +1,22 @@
 <script>
 	let name = 'James';
+	let color = 'green';
+
 	const handleClick = () => {
-		name = 'KYLE';
-	}
+		color = 'red';
+	};
+
+	const handleInput = (e) => {
+		color = e.target.value;
+	};
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Polls</p>
+	<p style="color: {color}">{color}</p>
 	<button on:click={handleClick}>UPDATE</button>
+	<!-- <input type="text" on:input={handleInput} value={color}/> -->
+	<input type="text" bind:value={color}>
 </main>
 
 <style lang="scss">
