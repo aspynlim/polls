2a89eeef906fa1916fb0b53bca6c2d89b71adbb5
JenLim <cmdlhz@gmail.com>
2021-07-24 17:09:34 +0900

    Conditionals

--- polls/src/App.svelte
+++ polls/src/App.svelte
@@ -9,11 +9,24 @@
 		people = people.filter((person) => person.id != id);
 		console.log(e);
 	};
+
+	let num = 5;
 </script>
+
+{#if num > 20}
+	<p>Graeter than 20</p>
+{:else if num > 5}
+	<p>Greater than 5</p>
+{:else}
+	<p>Default</p>
+{/if}
 
 <main>
 	{#each people as person (person.id)}
 		<div>
+			{#if person.color === 'red'}
+				<p><strong>RESULT : {person.color}</strong></p>
+			{/if}
 			<h4>{person.name}</h4>
 			<p>{person.age}</p>
 			<button on:click={(e) => handleClick(e, person.id)}>DELETE</button>
